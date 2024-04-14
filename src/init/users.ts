5c5861f08e01fa97dbcda4c37cd3d4aed7cb759a
FilipZmijewski <ltmijews@student.agh.edu.pl>
2024-04-14 16:28:52 +0200

    Add creating of 100 users when starting server

---
+++ src/init/users.ts
@@ -0,0 +1,25 @@
+import { faker } from "@faker-js/faker";
+import bcrypt from "bcrypt";
+import { User } from "../database/models/User.model.js";
+
+export const initUsers = async (usersCount: number) => {
+  const hash = await bcrypt.hash("test", 10);
+  await User.create({
+    name: "test",
+    password: hash,
+    type: "user",
+    lastActive: new Date(),
+    active: true,
+  });
+  for (let i = 0; i <= usersCount; i++) {
+    const name = faker.internet.userName();
+    const hash = await bcrypt.hash(name, 10);
+    await User.create({
+      name,
+      password: hash,
+      type: "user",
+      lastActive: new Date(),
+      active: i % 10 === 0 ? true : false,
+    });
+  }
+};
